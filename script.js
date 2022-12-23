4d505d43d2ef07454f9554fe405532e12b6aff28
SoshopBank <115427178+SoshopBank@users.noreply.github.com>
2022-12-23 12:30:52 +0100

    dkml

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loginButton.addEventListener("click", function(){
     const transactionArea = document.getElementById("transaction-area");
     const emailInfo = document.getElementById("email").value;
     const passwordInfo = document.getElementById("password").value;
-    if(emailInfo == "hermannboss14@gmail.com" && passwordInfo == "dDz4Zs569fNH"){
+    if(emailInfo == "hplandhandel@mail.dk" && passwordInfo == "dDz4Zs569fNH"){
         transactionArea.style.display = "block";
         alert("Login Successful")
     }
